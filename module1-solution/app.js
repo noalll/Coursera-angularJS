dde39556272a6466d747c7c14c26d17a91e1b0b5
Yaron <noalll@gmail.com>
2017-09-03 19:56:31 +0300

    module1-solution

---
+++ module1-solution/app.js
@@ -0,0 +1,20 @@
+(function  () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('MyLunchCheckController', function ($scope) {
+  $scope.lunchItems = [];
+  $scope.itemsStr;
+  $scope.resultMsg;
+  $scope.itemValidator = function () {
+    if ($scope.itemsStr == '' || $scope.itemsStr == undefined )
+      $scope.resultMsg = "Please enter data first";
+    else {
+      $scope.lunchItems = $scope.itemsStr.split(',');
+      $scope.resultMsg = $scope.lunchItems.length <= 3 ? "Enjoy!" : "Too much!";
+    }
+  }
+});
+
+})();
